ebda00b11b0e5202fb6dfbba6513762dc9e533b1
Jae Yong Kim <kim900180@gmail.com>
2018-07-12 06:30:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var listItem = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  for (var i = 0; i < listItem.length; i++) {
+    listItem[i].innerHTML = parseInt(listItem[i].innerHTML) + n
+  }
+  return listItem
+}
+
+function deepestChild() {
+  var listItem = document.getElementById('grand-node')
+}
